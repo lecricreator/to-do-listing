33b5419cdb5b61a4cc60a4e861c8eced2cf84cb4
Louca <loucamorales@orange.fr>
2026-02-06 00:43:52 +0100

    create a new file and remove the task you choose withe the index.

--- src/action/remove.rs
+++ src/action/remove.rs
@@ -1,4 +1,5 @@
-use std::{fmt::{Error, format}, i32, io::{self, BufRead, BufReader, Write}};
+use std::{i32, io::{self, BufRead, BufReader, Write}};
+use std::fs::{File};
 use crate::gestionary_file::{self};
 
 pub fn remove(argc: usize, args: &Vec<String>) -> io::Result<()>{
@@ -14,34 +15,43 @@ pub fn remove(argc: usize, args: &Vec<String>) -> io::Result<()>{
         }
     };
     let reader = BufReader::new(&file);
-    let mut index = -1;
+    let mut index = 0;
     let mut table_line: Vec<String> = vec![];
     for line in reader.lines() {
-        let line= line?;
-        if index > 1 {
-            print!("{} : ", index - 2)
+        let mut line= line?;
+        if index > 2 {
+            print!("{} : ", index - 3)
         }else {
             print!("    ");
         }
         println!("{}", line);
+        line += "\n";
         table_line.push(line);
         index += 1;
     }
-    index -= 3;
+    index -= 4;
     println!("Choose the index to remove. Ex 1");
     let mut input = String::new();
     std::io::stdin()
         .read_line(&mut input)
         .expect("Can not read user input");
-    let transf_input_to_int: i32 = match input.trim().parse::<i32>() {
+    let transf_input_to_int: usize = match input.trim().parse::<usize>() {
         Ok(i) => i,
         Err(e) => {
             eprintln!("Invalid number: {}", e);
             return Ok(());
         }
     };
-    if transf_input_to_int > index {
-        println!("value out of index of the to-do-rustlist.");
+    if transf_input_to_int > index {println!("value out of index of the to-do-rustlist."); return Ok(());}
+    let mut file_at_replace:File = File::options()
+    .write(true)
+    .create(true)
+    .open("replace_file")
+    .expect("Cannot create the replace_file.");
+    for t in 0..table_line.len(){
+        if t != transf_input_to_int + 3 {
+            file_at_replace.write(table_line[t].as_bytes()).expect("Can not write in file");
+        }
     }
     return Ok(())
 }
